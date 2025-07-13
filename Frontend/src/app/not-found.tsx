1da87c498c7497051a03cc544523e5abb1fb605f
Marlon Hernández <mmarlon.ssantiago@gmail.com>
2025-07-12 20:56:31 -0500

    Fixed ssr problems

--- Frontend/src/app/not-found.tsx
+++ Frontend/src/app/not-found.tsx
@@ -1,15 +1,28 @@
-import fourzerofour from "../app/assets/404.webp"
-import Image from "next/image"
-import Link from "next/link"
+import fourzerofour from "../app/assets/404.webp";
+import Image from "next/image";
+import Link from "next/link";
+
 const NotFound = () => {
     return (
-        <div className="min-h-screen bg-[#141313] flex flex-col justify-center items-center">
-            <Image src={fourzerofour} alt="Logo" className="md:w-[25%] w-[70%]" />
-            <p className="font-bold text-2xl text-center md:text-4xl text-white">The page you are looking for does not exist!</p>
-            <Link className="md:w-[20%] w-[90%] h-[50%] shrink-0 softhover mt-5 !tracking-normal btn negative hardhover text-center p-2" href="/">Take me home</Link>
-        </div>
+        <main className="min-h-screen bg-[#141313] flex flex-col justify-center items-center px-4">
+            <Image
+                src={fourzerofour}
+                alt="Illustration of 404 error - Page not found"
+                className="md:w-[25%] w-[70%]"
+                priority
+            />
+            <p className="font-bold text-2xl md:text-4xl text-center text-white mt-6">
+                The page you are looking for does not exist!
+            </p>
+            <Link
+                href="/"
+                className="md:w-[20%] w-[90%] mt-5 btn negative softhover hardhover text-center py-3 px-4 !tracking-normal shrink-0"
+                aria-label="Take me home!"
+            >
+                Take me home
+            </Link>
+        </main>
+    );
+};
 
-    )
-}
-
-export default NotFound
+export default NotFound;
