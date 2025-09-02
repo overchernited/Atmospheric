1ae68e83b69858ecfc8c05bd83071ad1a5d3fab4
Marlon Hernández <mmarlon.ssantiago@gmail.com>
2025-09-01 22:02:40 -0500

    PWA added

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
+const withPWA = require("next-pwa")({
+  dest: "public",      // carpeta donde se generará el service worker
+  register: true,      // registrar automáticamente el worker
+  skipWaiting: true,   // activar el nuevo worker inmediatamente
+});
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['apvbruuzlfnzfamjisoo.supabase.co'],
+    domains: ["apvbruuzlfnzfamjisoo.supabase.co"],
   },
+  // otras configuraciones de Next.js que tengas...
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
