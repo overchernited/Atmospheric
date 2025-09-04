dd2ee2efde712cefab00fa05b9285bc6348a0fd0
Marlon Hernández <mmarlon.ssantiago@gmail.com>
2025-09-03 21:11:39 -0500

    Function name changued to semantic improves

--- Frontend/src/app/dashboard/ecopoints/page.tsx
+++ Frontend/src/app/dashboard/ecopoints/page.tsx
@@ -6,20 +6,21 @@ import { useModal } from "@/hooks/Modal/useModal";
 import { useEffect } from "react";
 import PageBackground from "@/hooks/Background";
 import AddPoint from "./modals/AddPoint";
+import Btn from "@/components/Btn";
 
 const Map = dynamic(() => import("./components/MapComponent"), { ssr: false });
 const EcoPointsPage = () => {
     const { openModal } = useModal();
 
-    const handleOpenLogin = () => {
+    const handleAddPoint = () => {
         openModal("Add Point", <AddPoint />);
     };
 
     return (
         <PageBackground>
+            <Btn btnStyle="positive" className="z-[1000] tracking-normal! left-1/2 -translate-x-1/2 fixed! bottom-[10%] md:bottom-[0] mb-5" onClick={handleAddPoint}>Add Point</Btn>
             <div className="flex flex-col justify-center items-center text-2xl h-[80%] w-full">
                 <Map />
-                <button onClick={handleOpenLogin}>Login</button>
             </div>
         </PageBackground>
     );
